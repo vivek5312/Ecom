58527a8ab49def0a04876d59d08243a9a67c1009
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-09-02 10:00:38 +0530

    cart user specific

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState,useContext } from 'react';
+import React, { useState,useContext} from 'react';
 import './App.css';
 import data from './components/Back/Data/Data';
 import Header from './components/Font/Header/Header';
@@ -20,6 +20,8 @@ function App() {
   const [cartShown, setCartShown] = useState(false);
   const [cartItem, setCartItem] = useState([]);
 
+
+  
   const shownCartHandler = () => {
     setCartShown(true);
   };
@@ -28,26 +30,53 @@ function App() {
     setCartShown(false);
   };
 
-  const handleAddProduct=(product)=>{
-    const ProductExist=cartItem.find((item)=>item.id===product.id);
-    if(ProductExist){
-      setCartItem(cartItem.map((item)=>item.id===product.id ? {...ProductExist,quantity:ProductExist.quantity+1}:item))
-    }
-   else{
-    setCartItem([...cartItem,{...product,quantity:1}])
-   }
+ const handleAddProduct = (product) => {
+  const ProductExist = cartItem.find((item) => item.id === product.id);
+ 
+  
+  if (ProductExist) {
+    setCartItem((prevCartItems) =>
+      prevCartItems.map((item) =>
+        item.id === product.id
+          ? { ...ProductExist, quantity: ProductExist.quantity + 1 }
+          : item
+      )
+    );
+    
+  } else {
+    setCartItem([...cartItem, { ...product, quantity: 1 }]);
+    
+  }
+ 
+
+};
+
+
+const handleRemoveProduct = (product) => {
+  const ProductExist = cartItem.find((item) => item.id === product.id);
+  let updatedCart;
+
+  if (ProductExist.quantity === 1) {
+    setCartItem((prevCartItems) => prevCartItems.filter((item) => item.id !== product.id));
+    updatedCart = cartItem.filter((item) => item.id !== product.id);
+  } else {
+    setCartItem((prevCartItems) =>
+      prevCartItems.map((item) =>
+        item.id === product.id
+          ? { ...ProductExist, quantity: ProductExist.quantity - 1 }
+          : item
+      )
+    );
+    updatedCart = cartItem.map((item) =>
+      item.id === product.id
+        ? { ...ProductExist, quantity: ProductExist.quantity - 1 }
+        : item
+    );
   }
 
-  const handleRemoveProduct=(product)=>{
-    const ProductExist=cartItem.find((item)=>item.id===product.id);
-    if(ProductExist.quantity===1){
-      setCartItem(cartItem.filter((item)=>item.id!==product.id));
-
-    }
-    else{
-      setCartItem(cartItem.map((item)=>item.id===product.id?{...ProductExist,quantity:ProductExist.quantity-1}:item));
-    }
-  }
+  // Update local storage after setting state
+  localStorage.setItem("cartItem", JSON.stringify(updatedCart));
+};
 const handleCartClearance=()=>{
   setCartItem([]);
 }
@@ -72,6 +101,7 @@ async function addDetail(detail) {
     // You can add further error handling or display error messages to the user here.
   }
 }
+  
 
 
   return (
---
+++ src/AuthPages.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AuthForm from '../../../../Auth/Authform'
+
+const AuthPages = () => {
+  return (
+    <>
+    <h1 className='head-title'>Electronics Shop</h1>
+    <AuthForm/>
+    <footer>
+    <h1 className='foot'>The Electronics Shop</h1>
+</footer>
+    </>
+  )
+}
+
+export default AuthPages
